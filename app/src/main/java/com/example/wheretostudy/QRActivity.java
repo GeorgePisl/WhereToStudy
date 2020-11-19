2d4ebdfadf6b896a6a30d156462759ca3de45720
Herson <ruiz.1643944@studenti.uniroma1.it>
2020-11-19 13:04:25 +0100

    Added QR Scanner

--- app/src/main/java/com/example/wheretostudy/QRActivity.java
+++ app/src/main/java/com/example/wheretostudy/QRActivity.java
@@ -3,78 +3,194 @@ package com.example.wheretostudy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ScanMode;
 import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import javax.xml.transform.Result;
+
 public class QRActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener authStateListener;
+    CodeScanner mCodeScanner;
+    private final static int CAMERA_REQUEST_CODE = 101;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                CAMERA_REQUEST_CODE);
+    }
+
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(QRActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_activity_qr);
+        if (checkPermission()) {
+            CodeScannerView scannerView = findViewById(R.id.scanner_view);
+            mCodeScanner = new CodeScanner(this, scannerView);
+            mCodeScanner.setCamera(CodeScanner.CAMERA_BACK);
+            mCodeScanner.setFormats(CodeScanner.ALL_FORMATS);
+            mCodeScanner.setAutoFocusMode(AutoFocusMode.SAFE);
+            mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
+            mCodeScanner.setAutoFocusEnabled(true);
+            mCodeScanner.setFlashEnabled(false);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ImageView menu = toolbar.findViewById(R.id.lateralMenu);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-                drawerLayout.openDrawer(Gravity.LEFT);
-            }
-        });
-
-        mAuth = FirebaseAuth.getInstance();
-
-        //part for navigation drawer
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.getMenu().getItem(2).setChecked(true);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.nav_home){
-                    item.setChecked(false);
-                    Intent myIntent = new Intent(QRActivity.this, MapActivity.class);
-                    QRActivity.this.startActivity(myIntent);
-
+            mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                @Override
+                public void onDecoded(@NonNull com.google.zxing.Result result) {
+                    QRActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(QRActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
-                else if (item.getItemId() == R.id.nav_chat){
-                    item.setChecked(false);
-                    Intent myIntent = new Intent(QRActivity.this, ChatActivity.class);
-                    QRActivity.this.startActivity(myIntent);
+            });
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCodeScanner.startPreview();
                 }
-                else if (item.getItemId() == R.id.nav_logout){
-                    item.setChecked(false);
-                    mAuth.signOut();
-                    FacebookSdk.sdkInitialize(getApplicationContext());
-                    LoginManager.getInstance().logOut();
-                    AccessToken.setCurrentAccessToken(null);
-                    finish();
-                    startActivity(new Intent(QRActivity.this, Login.class));
-                    if (authStateListener != null) {
-                        mAuth.removeAuthStateListener(authStateListener);
+            });
+
+
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            ImageView menu = toolbar.findViewById(R.id.lateralMenu);
+            menu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+                    drawerLayout.openDrawer(Gravity.LEFT);
+                }
+            });
+
+            mAuth = FirebaseAuth.getInstance();
+
+            //part for navigation drawer
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.getMenu().getItem(2).setChecked(true);
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.nav_home) {
+                        item.setChecked(false);
+                        Intent myIntent = new Intent(QRActivity.this, MapActivity.class);
+                        QRActivity.this.startActivity(myIntent);
+
+                    } else if (item.getItemId() == R.id.nav_chat) {
+                        item.setChecked(false);
+                        Intent myIntent = new Intent(QRActivity.this, ChatActivity.class);
+                        QRActivity.this.startActivity(myIntent);
+                    } else if (item.getItemId() == R.id.nav_logout) {
+                        item.setChecked(false);
+                        mAuth.signOut();
+                        FacebookSdk.sdkInitialize(getApplicationContext());
+                        LoginManager.getInstance().logOut();
+                        AccessToken.setCurrentAccessToken(null);
+                        finish();
+                        startActivity(new Intent(QRActivity.this, Login.class));
+                        if (authStateListener != null) {
+                            mAuth.removeAuthStateListener(authStateListener);
+                        }
                     }
+                    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
                 }
-                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-                drawerLayout.closeDrawer(GravityCompat.START);
-                return true;
-            }
-        });
+            });
+        } else {
+            requestPermission();
+        }
     }
+
+
 }
